63ef3b30358b52119cc4ae9bc565ccb4a37c38f9
teyang-lau <ty_lau@outlook.com>
2023-05-23 15:58:33 +0800

    import prompts from external file

--- scripts/text2markdown.py
+++ scripts/text2markdown.py
@@ -1,53 +1,12 @@
 import openai
+from .prompt_templates import SYSTEM_CONTENT_LECTURES, USER_CONTENT_LECTURES, SYSTEM_CONTENT_MEETINGS, USER_CONTENT_MEETINGS
 
-SYSTEM_CONTENT = """You are an assistant that only speaks in Markdown. Do not write text that isn't formatted as markdown.
-
-Example formatting:
-
-<h1 style="text-align: center;">YOListenO</h1>
-
-## Summary
-
-This audio recording documents a test of a workflow using AI to transcribe lecture audios to markdown formatted notes.
-
-## Main Points
-
-- point 1
-- point 2
-
-## Action Items
-
-- point 1
-- point 2
-
-## Follow Up Questions
-
-- point 1
-- point 2
-
-## Potential Arguments
-
-- point 1
-- point 2"""
-
-USER_CONTENT = """Write a Title for the transcript that is under 15 words and center it using <h1 style="text-align: center;">Title</h1>
-
-Write "Summary" as a Heading 1.
-
-Write a summary of the provided transcript.
-
-Then return a list of the main points in the provided transcript. Then return a list of action items. Then return a list of follow up questions. Then return a list of potential arguments against the transcript.
-
-For each list, return a Heading 2 before writing the list items. Limit each list item to 100 words, and return no more than 5 points per list.
-
-Transcript:
-"""
 
 def text2markdown(
     transcript: str,
     api_key: str,
-    system_content: str=SYSTEM_CONTENT,
-    user_content: str=USER_CONTENT,
+    system_content: str=SYSTEM_CONTENT_LECTURES,
+    user_content: str=USER_CONTENT_LECTURES,
     temperature: float=0.2,
 
 
