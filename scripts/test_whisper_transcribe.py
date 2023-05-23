60b63648514144e16d2d36e5da502e835a955b1e
teyang-lau <ty_lau@outlook.com>
2023-05-23 13:52:50 +0800

    first commit

---
+++ scripts/test_whisper_transcribe.py
@@ -0,0 +1,20 @@
+from whisper_transcribe import wtranscribe
+
+
+def test_can_transcribe_to_text():
+    result = wtranscribe("base", "../samples/audio/sample2.mp3")
+    assert isinstance(result, str)
+
+
+# def test_can_get_same_temperature_output():
+#     result = wtranscribe("base", "../samples/audio/sample2.mp3", temperature=0.2)
+#     result1 = wtranscribe("base", "../samples/audio/sample2.mp3", temperature=0.2)
+
+#     assert result.lower() == result1.lower()
+
+
+# def test_can_get_different_temperature_output():
+#     result = wtranscribe("base", "../samples/audio/sample2.mp3", temperature=0.2)
+#     result1 = wtranscribe("base", "../samples/audio/sample2.mp3", temperature=0.8)
+
+#     assert result.lower() != result1.lower()
